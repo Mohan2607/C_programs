15e0f277250653d4a673a2ce6fd192d1dc4378d3
Mohan2607 <70357994+Mohan2607@users.noreply.github.com>
2021-03-12 10:36:32 +0530

    Add files via upload

---
+++ counting occurence of  'the' in a sentence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i,a,count=0;
+	char str[100];
+	printf("enter the string:");
+	gets(str);
+	a=strlen(str);
+	for(i=0;i<=a;i++){
+		if((str[i]=='t' || str[i]=='T')&&(str[i+1]=='h' || str[i+1]=='H')&&(str[i+2]=='e' || str[i+2]=='E'))
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+	
+}
